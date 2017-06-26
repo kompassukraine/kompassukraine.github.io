f573d5e014afeb4350c15b9057e4fb57d192a402
kompassukraine <marketing@kompass.ua>
2017-06-26 13:33:00 +0300

    Create kompass_com.js

---
+++ js/kompass_com.js
@@ -0,0 +1,118 @@
+document.write('<style>header .votrecompte .cart_header_panel span i, header .votrecompte .myMiniAccount span i {margin: 0!important;} #sh_button {border-radius: 5px 5px 0 0; box-shadow: 0 5px 10px 6px rgba(146, 150, 151, 0.2);}.sh_title_text{padding:0 25px !important;font-family: Roboto !important;font-size: 18px !important;font-weight: 700 !important;} #Toplink {margin-bottom:10px;} #Toplink a {display: block;padding: 10px;text-decoration: none;background:none repeat scroll 0 0 #2c82c9; color:#ffffff; font-size:18px; font-weight:700px;} #Toplink a:hover {background:none repeat scroll 0 0 #207cad;} /*.classificationListCategoryLeft {width: 310px;} .classificationListCompany .filtres .nav_column .refinementToggle, .classificationListCategoryLeft ul, .classificationListCompany .filtres {width: 300px;} .classificationListCategoryMiddle, .classificationListCategory .prod_list,  #content .classificationListCategoryMiddle .resultats, .classificationListCategoryRight {width: 460px;} .classificationListCompany .classificationListCategoryRight {width: 120px;} .classificationListCategoryLeft img {  height: 196px;  width: 300px;} */ .classificationResultFilter .classificationCounterResult {width:auto;} #Toplink span:first-child, .page-search .publicite, .pub-container.large,  #completeEnterpriseLink1 > br, .headerBanner {display:none;} .page-productDetails .editMyCompany i, .page-productDetails .editMyCompany p {color: #e5312b!important;} .phone > p#phone {cursor: default !important;}  .buyCompanyDataUA ul { bottom: 0; margin: 0; } .buyCompanyDataUA li { background: none repeat scroll 0 0 #fff; cursor: pointer; display: table; font-weight: 700; margin-bottom: 10px; padding: 0; position: relative; table-layout: fixed; width: 300px; border: solid 1px #e5312b !important;} .buyCompanyDataUA li a, .buyCompanyDataUA li a:hover { text-decoration: none; display: block;} .buyCompanyDataUA li a:hover { background: none repeat scroll 0 0 #f3f3f3; } .buyCompanyDataUA p { display: table-cell; font-size: 16px; font-weight: 400; line-height: 1.2; margin: 0; padding: 7px 10px; vertical-align: middle; width: 230px; } .buyCompanyDataUA li span { background: url("http://ua.kompass.com/_ui/desktop/theme-kompass/images/ficheEntre/cart-icon.png") no-repeat scroll 0 0 #e5312b; display: table-cell; height: 50px; text-align: center; vertical-align: middle; width: 50px; }  h1.entry-title {text-indent: 0;top: 0;position:relative;} .single-thumbnail {text-align: center;} #recent-comments-2, #archives-2, .entry-meta, .classificationListCategoryRightBanner, #homepage-container-2-ad > a {display:none;} .classificationListCategory .prod_list .nomen {width: 87%;} .headerBannerUA {text-align:center;} @media only screen and (max-width : 730px) {.headerBannerUA {display:none!important;}} #rightColContactCompany {padding: 0 0 20px!important;} #rightColContactCompany.rightColFixed {width: 300px!important;} a#backtotop {bottom: 3px;right: 46px;} #uaTop {cursor: default!important;height:642px!important;padding:0!important;} #uaTop .contentTop {padding:20px 20px 0 20px;} #uaTop .Zagolovok {background: #d7002c;text-align:center;font-size: 20px;padding: 15px 5px;width:100%;color:white;} #uaTop .ZagolovokGreen {background: #8dc63f;text-align:center;font-size: 20px;padding: 15px 5px;width:100%;color:white;} #uaTop .ZagolovokDark {background: #222e3a;text-align:center;font-size: 20px;padding: 15px 5px;width:100%;color:white;} #uaTop h2 {text-align:center; color:#337ab7!important;font-weight: 500!important;padding: 20px 0 !important;} #uaTop p {line-height: 1.5!important;font-size: 14px!important;} #uaTop p a {color:#0084ff!important;} #uaTop .topImg {text-align:center;height:120px;} #uaTop .topImg img {max-height:120px!important;} #uaTop .list-buttons-container {top:450px!important;} #uaTop .price {text-align:center; color: #ef8700;font-size: 20px;margin-bottom: 15px;width: 90%;} #uaTop .blueButton a {border: 1px solid #dadada;    border-radius: 5px; display: inline-block; text-align:center;background-color: #0084ff; padding:10px 5px;width: 90%;font-weight: 600;color:white;} #uaTop .blueButton a:hover {border: 1px solid #0084ff;    background-color: white; color:#d7002c;} #uaTop .whiteButton a {border: 1px solid #0084ff;    border-radius: 5px; display: inline-block; text-align:center;background-color: white; padding:10px 5px;width: 90%;font-weight: 600;color:#0084ff;} #uaTop .whiteButton a:hover {color:#d7002c;} </style>');
+function GoogleKompass() {
+var script = document.createElement('script');
+script.type = 'text/javascript';
+script.async = true;
+script.src = '//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js';
+document.body.appendChild(script);
+
+(adsbygoogle = window.adsbygoogle || []).push({});
+};
+
+	$( "#wrapper" ).before( "<div class='headerBannerUA'><div style='height:90px;width:728px; margin:7px auto; background: none repeat scroll 0 0 #ecf7fb;'><ins class='adsbygoogle' style='display:inline-block;width:728px;height:90px' data-ad-client='ca-pub-3803978893532423' data-ad-slot='7746872834'></ins></div></div>" );
+	GoogleKompass();
+
+var razdellink = window.location.pathname;
+var razdel = razdellink.split("/");
+
+switch (razdel[1]) {
+case "searchCompanies":
+	{
+	$( ".publicite:eq(0)" ).before( "<div style='height:600px; width:300px;border: 1px dashed #e5e5e5; background: none repeat scroll 0 0 #ecf7fb;'><ins class='adsbygoogle' style='display:inline-block;width:300px;height:600px' data-ad-client='ca-pub-3803978893532423' data-ad-slot='1330348349'></ins></div>" );
+	GoogleKompass();		
+	$( ".prod_list:eq(6)" ).before( "<div class='prod_list is-booster cc_col-lg-12 cc_col-md-12 cc_col-sm-12 cc_col-xs-12'><ins class='adsbygoogle' style='display:block' data-ad-format='autorelaxed' data-ad-client='ca-pub-3803978893532423'      data-ad-slot='7626125541'></ins></div>" );
+	GoogleKompass();
+	//$( "#search_filters_div .item:eq(3)" ).after( "<div  class='item' style='height:250px; width:300px;border: 1px dashed #e5e5e5; background: none repeat scroll 0 0 #ecf7fb'><ins class='adsbygoogle' style='display:inline-block;width:300px;height:250px'     data-ad-client='ca-pub-3803978893532423' data-ad-slot='1047059549'></ins></div>" );
+	//GoogleKompass();
+	};
+	break;
+case "a":
+case "s":
+case "r":
+	{
+	$( ".classificationListCategoryRightBanner:eq(0)" ).before( "<div style='height:600px; width:300px;border: 1px dashed #e5e5e5; background: none repeat scroll 0 0 #ecf7fb;margin: 0 0 23px;' ><ins class='adsbygoogle' style='display:inline-block;width:300px;height:600px' data-ad-client='ca-pub-3803978893532423' data-ad-slot='1330348349'></ins></div>" );
+	GoogleKompass();
+	$( ".prod_list:eq(3)" ).before( "<div class='prod_list' style='width:100%;height:auto!important;'><ins class='adsbygoogle' style='display:block' data-ad-format='autorelaxed' data-ad-client='ca-pub-3803978893532423'      data-ad-slot='7626125541'></ins></div>" );
+	GoogleKompass();	
+	//$( ".filtres" ).before( "<div class='buyCompanyDataUA'><ul><li><a href='http://on.kompass.ua/NewKompassUkraine'><p>Купить базу данных компаний Украины</p><span class='cart-icon'></span></a></li><li><a href='http://on.kompass.ua/EasyBusinessPack'><p>Купить онлайн-доступ к базе данных 68 стран мира</p><span class='cart-icon'></span></a></li></ul></div>" );
+	//$( ".filtres" ).after( "<div  class='filtres' style='height:250px; width:300px;border: 1px dashed #e5e5e5; background: none repeat scroll 0 0 #ecf7fb'><ins class='adsbygoogle' style='display:inline-block;width:300px;height:250px'     data-ad-client='ca-pub-3803978893532423' data-ad-slot='1047059549'></ins></div>" );
+	//GoogleKompass();	
+	};
+	break;
+case "c":
+
+	{
+	$( "#activities" ).before( "<div class='item' style='width:100%;height:auto!important;padding:0px!important;'><ins class='adsbygoogle' style='display:block' data-ad-format='autorelaxed' data-ad-client='ca-pub-3803978893532423'      data-ad-slot='7626125541'></ins></div>" );
+	GoogleKompass();		
+	//$( ".pull-left" ).after( "<div style='position:absolute; right:-140px; z-index:99; height:600px; width:120px;border: 1px dashed #e5e5e5; background: none repeat scroll 0 0 #ecf7fb'><ins class='adsbygoogle' style='display:inline-block;width:120px;height:600px' data-ad-client='ca-pub-3803978893532423' data-ad-slot='4460014342'></ins></div>" );
+	//GoogleKompass();
+	$( "#rightColContactCompany #contactCompanyTitle" ).before( "<div style='height:250px; width:300px; background: none repeat scroll 0 0 #ecf7fb; margin-bottom: 15px; padding:0px'><ins class='adsbygoogle' style='display:inline-block;width:300px;height:250px'     data-ad-client='ca-pub-3803978893532423' data-ad-slot='1047059549'></ins></div>" );
+	GoogleKompass();	
+	};
+	break;
+case "b":
+	{
+	$( "#homepage-container-2-ad" ).before( "<div id='homepage-container-2-ad' class='cc_col-xs-12'><ins class='adsbygoogle' style='display:inline-block;width:300px;height:250px'     data-ad-client='ca-pub-3803978893532423' data-ad-slot='1047059549'></ins></div>" );
+	GoogleKompass();
+	$( "#homepage-container-1-companies" ).after( "<ul id='homepage-container-1-companies' class='cc_row'><li class='cc_col-xs-12 cc_col-sm-12'><ul><li class='cc_col-md-12 cc_col-sm-12 cc_col-xs-12'><div class='seo_content'><div style='width:100%;height:auto!important;padding:0px!important;'><ins class='adsbygoogle' style='display:block' data-ad-format='autorelaxed' data-ad-client='ca-pub-3803978893532423'      data-ad-slot='7626125541'></ins></div></div></li></ul></li></ul>" );
+	GoogleKompass();	
+	};
+	break;	
+case "e":
+	{
+	$( "#recent-posts-2" ).before( "<aside class='widget' style='height:250px; width:300px; background: none repeat scroll 0 0 #ecf7fb; margin-bottom: 15px; padding:0px'><ins class='adsbygoogle' style='display:inline-block;width:300px;height:250px'     data-ad-client='ca-pub-3803978893532423' data-ad-slot='1047059549'></ins></aside>" );
+	GoogleKompass();
+	$( "#categories-2" ).before( "<aside class='widget' style='height:250px; width:300px; background: none repeat scroll 0 0 #ecf7fb; margin-bottom: 15px; padding:0px'><ins class='adsbygoogle' style='display:inline-block;width:300px;height:250px'     data-ad-client='ca-pub-3803978893532423' data-ad-slot='1047059549'></ins></aside>" );
+	GoogleKompass();	
+	};
+	break;	
+
+};
+switch (razdel[1]) {
+case "searchCompanies":
+case "a":
+case "s":
+case "r":
+	{
+			//$( "#resultatDivId .prod_list:eq(0)" ).before( "<div class='prod_list is-booster uaTop cc_col-lg-4 cc_col-md-6 cc_col-sm-6 cc_col-xs-12'><ins class='adsbygoogle' style='display:block' data-ad-client='ca-pub-3803978893532423' data-ad-slot='7846451544' data-ad-format='auto'></ins></div>");
+			
+			//$( "#resultatDivId .prod_list:eq(0)" ).before( "<div class='prod_list is-booster uaTop cc_col-lg-4 cc_col-md-6 cc_col-sm-6 cc_col-xs-12'><div style='height:600px; width:300px;border: 1px dashed #e5e5e5; background: none repeat scroll 0 0 #ecf7fb;'><ins class='adsbygoogle' style='display:inline-block;width:300px;height:600px' data-ad-client='ca-pub-3803978893532423' data-ad-slot='1330348349'></ins></div></div>");
+			
+			$( "#resultatDivId .prod_list:eq(0)" ).before( "<div id='uaTop' class='prod_list  cc_col-lg-4 cc_col-md-6 cc_col-sm-6 cc_col-xs-12'><div class='contentTop'><div class='ZagolovokDark'>Добавить компанию</div><div class='infos'><div class='details'><h2>Ваша компания на портале Компасс</h2><p style='margin-bottom:20px;'>Разместите <a href='/registerNewCompany/identity' target='_blank'><b>БЕСПЛАТНО</b></a> информацию о Вашей компании на Международном B2B-портале Kompass.com<br /></p></div><div class='blueButton'><a href='/registerNewCompany/identity' target='_blank' style='width: 100%!important;'>Добавить компанию</a></div> </div> </div><div  style='margin-top: 30px; padding-top: 27px; border-top:1px solid #e1e1e1;height:250px;width:100%;text-align:center;'><ins class='adsbygoogle' style='display:inline-block;width:300px;height:250px'     data-ad-client='ca-pub-3803978893532423' data-ad-slot='1047059549'></ins></div></div>" );
+			GoogleKompass();
+			
+			$( "#resultatDivId .prod_list:eq(1)" ).before( "<div id='uaTop' class='prod_list cc_col-lg-4 cc_col-md-6 cc_col-sm-6 cc_col-xs-12'><div class='contentTop'><div class='Zagolovok'>Скачайте базы данных</div><div class='infos'><div class='details'><div class='topImg'><img src='http://prokcssdata.blob.core.windows.net/mediacontent/kompass_Ukraine.png' alt='Справочник «Компасс Украина»'></div><h2>Базы данных компаний Украины</h2><p>Более <b>51.100 украинских компаний</b> всех видов деятельности в электронных справочниках «Компасс».<br /> Скачайте <a href='http://demo.kompass.ua/' target='_blank'><b>бесплатную демоверсию</b></a> и ознакомьтесь со всеми возможностями коммерческой версии!</p></div><div class='list-buttons-container'><div class='price'>от <b>1900 грн</b></div><div class='blueButton'><a href='http://demo.kompass.ua/' target='_blank'>Бесплатная демоверсия</a></div><div class='whiteButton'><a href='http://www.kompass.ua/demo/#about' target='_blank'>Узнать подробности</a> </div></div> </div> </div></div>" );
+			$( "#resultatDivId .prod_list:eq(2)" ).before( "<div id='uaTop' class='prod_list cc_col-lg-4 cc_col-md-6 cc_col-sm-6 cc_col-xs-12'><div class='contentTop'><div class='ZagolovokGreen'>Создайте базы данных</div><div class='infos'><div class='details'><div class='topImg'><img src='http://prokcssdata.blob.core.windows.net/mediacontent/EasyList.jpg' alt='Создайте базы данных'></div><h2>Базы данных 8 млн компаний 68 стран</h2><p>Создайте собственные <a href='/buy-company-list/' target='_blank'><b>базы компаний</b></a> по Вашим критериям: продукция или услуги, страна или регион, наличие определенных контактов, а также величина компании. К Вашим услугам базы данных <b>8 млн компаний 68 стран мира</b>!</p></div><div class='list-buttons-container'><div class='price'>от <b>5 грн</b></div><div class='blueButton'><a href='/buy-company-list/' target='_blank'>Создать базу данных</a></div><div class='whiteButton'><a href='/m/purchase-prospects-list/' target='_blank'>Узнать подробности</a> </div></div> </div> </div></div>" );			
+			
+	}
+	break;	
+
+};
+
+switch (razdel[1]) {
+case "registerNewCompany":
+	{
+	$( "aside" ).after( "<aside class='span4'><header>Видео-инструкция</header><div style='padding:10px'><iframe width='280' height='158' src='https://www.youtube.com/embed/sqg6g3RijMI?rel=0&amp;showinfo=0&amp;t=23s' frameborder='0' allowfullscreen></iframe></div></aside>" );
+	};
+	break;
+};
+
+
+
+!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+document,'script','https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1306045849460015');
+fbq('track', 'PageView');
+
+
+
+        (function(w,d,u,b){
+                s=d.createElement('script');r=(Date.now()/1000|0);s.async=1;s.src=u+'?'+r;
+                h=d.getElementsByTagName('script')[0];h.parentNode.insertBefore(s,h);
+        })(window,document,'https://cdn.bitrix24.ua/b3265019/crm/site_button/loader_2_988pxd.js');
+	//$( "h4.bx-imopenlines-config-sidebar-info-title-item" ).after( "!!!<h4>Здравствуйте!</h4>" );
+	document.write('<style>.bx-imopenlines-config-sidebar-logo {display:none!important;} .bx-imopenlines-config-sidebar-message-item {white-space: normal!important;}	</style>');
+
